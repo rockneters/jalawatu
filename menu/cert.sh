c8a7aaa78c709d382bf3ca96a64d870fb91b0443
rockneters <89226199+rockneters@users.noreply.github.com>
2021-09-23 12:22:41 +0700

    Add files via upload

--- menu/cert.sh
+++ menu/cert.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-MYIP=$(wget -qO- icanhazip.com);
-echo "Checking VPS"
-clear
 echo start
 sleep 0.5
-#source /var/lib/premium-script/ipvps.conf
-domain=$IP
+domain=$(cat /etc/v2ray/domain)
 systemctl stop v2ray
 systemctl stop v2ray@none
 /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
@@ -17,5 +10,3 @@ systemctl start v2ray
 systemctl start v2ray@none
 echo Done
 sleep 0.5
-clear 
-neofetch
